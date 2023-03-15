8995a3da68bcd0346a0203bc5131b29614539c4d
lpardey <lpardey@outlook.com>
2023-03-15 16:11:19 +0000

    add GResult, GStatistics classes and modif Gstate

--- w_game/game.py
+++ w_game/game.py
@@ -14,8 +14,9 @@ class PlayerState(BaseModel):
     attempts_left: int
 
 
-class GameResult(BaseModel):
-    result: list
+class GameResultEnum(str, Enum):
+    GUESSED = "GUESSED"
+    NOT_GUESSED = "NOT_GUESSED"
 
 
 class Guess(BaseModel):
@@ -27,6 +28,20 @@ class WordleLetters(BaseModel):
     letters_out_of_place: list[str]
     letters_not_in_word: list[str]
     letters_available: list[str]
+
+
+class GuessResult(BaseModel):
+    player_guess: Guess
+    game_letters: WordleLetters
+    game_result: GameResultEnum
+    current_streak: int
+
+
+class GameStatistics(BaseModel):
+    game_results: list[GuessResult]
+    games_played: int
+    winning_percentage: int
+    max_streak: int
 
 
 class GameStatus(str, Enum):
@@ -46,9 +61,22 @@ class GameState(BaseModel):
         letters_available=[],
     )
     status: GameStatus = GameStatus.AVAILABLE_TO_PLAY
+    statistics: GameStatistics = GameStatistics(
+        game_results=[
+            GuessResult(
+                player_guess=Guess(guess="random word"),
+                game_letters=WordleLetters(
+                    letters_in_place=[],
+                    letters_out_of_place=[],
+                    letters_not_in_word=[],
+                    letters_available=[],
+                ),
+                game_result=GameResultEnum.GUESSED,
+                current_streak=1,
+            )
+        ],
+        games_played=1,
+        winning_percentage=100,
+        max_streak=1,
+    )
     finished: bool = False
-
-
-class UserHistory(BaseModel):
-    game_count: int = 0
-    historic_results: list[GameResult] = []
