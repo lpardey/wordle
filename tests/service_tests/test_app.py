eee4525efd0b4a2b1ed99b98c8478e02f94234a1
lpardey <lpardey@outlook.com>
2023-03-15 16:09:49 +0000

    fix addgamestate and add getgame state/stats

--- tests/service_tests/test_app.py
+++ tests/service_tests/test_app.py
@@ -1,8 +1,8 @@
 from unittest import mock, TestCase
 import pytest
-from w_game.game import GameState, PlayerState, User
-from wordle.game.router import GameStorage, create_game
-from wordle_schemas.game import GameConfig, GameCreationResponse
+from w_game.game import GameState
+from wordle.game.router import GameStorage, create_game, get_game_state, get_game_stats
+from wordle_schemas.game import BasicStatus, GameConfig, GameCreationResponse
 
 
 # def test_create_game():
@@ -111,10 +111,37 @@ class TestCreateGame(TestCase):
     @mock.patch("wordle.game.router.GameStorage.storage_size", return_value=0)
     def test_add_game_state(self, m_storage_size: mock.Mock):
         game_storage = GameStorage()
-        game_state = GameState(guess="random word")
+        game_state = GameState()
         result = game_storage.add_game_state(game_state=game_state)
         expected_result = m_storage_size.return_value
         self.assertEqual(result, expected_result)
         assert m_storage_size.assert_called
 
 
+@mock.patch("wordle.game.router.GameStorage.get_game_state", return_value=GameState())
+def test_get_game_state(m_get_game_state: mock.Mock):
+    game_state: GameState = m_get_game_state.return_value
+    game_id = 0
+    response = get_game_state(game_id=game_id)
+    assert m_get_game_state.assert_called_once
+    assert m_get_game_state.call_args_list[0][1]["index"] == game_id
+    assert m_get_game_state.call_args == mock.call(index=game_id)
+    assert response.message is None
+    assert response.status == BasicStatus.OK
+    assert response.game_status_info.current_guess == game_state.guess
+    assert response.game_status_info.game_status == game_state.status
+    assert response.game_status_info.guess_letters == game_state.guess_letters
+
+
+@mock.patch("wordle.game.router.GameStorage.get_game_state", return_value=GameState())
+def test_get_game_stats(m_get_game_state: mock.Mock):
+    game_state: GameState = m_get_game_state.return_value
+    game_id = 0
+    response = get_game_stats(game_id=game_id)
+    assert m_get_game_state.assert_called_once
+    assert m_get_game_state.call_args_list[0][1]["index"] == game_id
+    assert m_get_game_state.call_args == mock.call(index=game_id)
+    assert response.game_statistics.game_results == game_state.statistics.game_results
+    assert response.game_statistics.games_played == game_state.statistics.games_played
+    assert response.game_statistics.winning_percentage == game_state.statistics.winning_percentage
+    assert response.game_statistics.max_streak == game_state.statistics.max_streak
