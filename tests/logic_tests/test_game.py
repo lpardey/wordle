55cafa41a2999d7187e9a43491b2fc575bc182b8
lpardey <lpardey@outlook.com>
2023-03-28 16:09:48 +0000

    refactor

--- tests/logic_tests/test_game.py
+++ tests/logic_tests/test_game.py
@@ -5,18 +5,6 @@ from wordle_game.game_state import GameResult, GameState, GameStatus, GuessResul
 from wordle_game.game import WordleException, WordleGame
 
 
-@pytest.fixture
-def basic_game_state():
-    game_state = GameState(user_id=0, game_word="PIZZA")
-    return game_state
-
-
-@pytest.fixture
-def basic_wordle_game(basic_game_state: GameState):
-    wordle_game = WordleGame(game_state=basic_game_state)
-    return wordle_game
-
-
 @pytest.mark.parametrize(
     "player_guess, expected_result",
     [
