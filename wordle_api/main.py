f098537451952aa46bd8150305c63454a5f783b6
ggutierrez-sunbright <ggutierrez@sunbright.tech>
2023-09-28 22:06:35 +0000

    add cors

--- wordle_api/main.py
+++ wordle_api/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from wordle_api.routers import game, player
 from wordle_api.user.router import router as user_router
 from wordle_api.auth.asdf import router as auth_router
 
+
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(game.router)
 app.include_router(player.router)
 app.include_router(auth_router)
