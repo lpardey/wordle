91f268b32ff363d213ea75d44b306a4fb8ff1b9d
lpardey <lpardey@outlook.com>
2023-10-03 00:43:09 +0000

    remove unnecessary router

--- wordle_api/main.py
+++ wordle_api/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from wordle_api.routers import game, player
+from wordle_api.routers import game
 from wordle_api.user.router import router as user_router
-from wordle_api.auth.asdf import router as auth_router
+# from wordle_api.auth.asdf import router as auth_router
 
 
 app = FastAPI()
@@ -17,7 +16,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.include_router(game.router)
-app.include_router(player.router)
-app.include_router(auth_router)
 app.include_router(user_router)
+app.include_router(game.router)
+# app.include_router(auth_router)
