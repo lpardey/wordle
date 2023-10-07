3cdd319f9cd2ffa50f24f9d85d543ad1997bbd6f
lpardey <lpardey@outlook.com>
2023-10-07 00:38:18 +0000

    refactor user session model

--- wordle_api/models/user_session.py
+++ wordle_api/models/user_session.py
@@ -1,11 +1,12 @@
 from tortoise import fields
 from tortoise.models import Model
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class UserSession(Model):
-    session_id = fields.IntField(pk=True)
-    user_id = fields.ForeignKeyField("models.User", related_name="sessions")
-    token = fields.CharField(max_length=20)
+    id = fields.IntField(pk=True)
+    user = fields.ForeignKeyField("models.User", related_name="sessions")
+    token = fields.CharField(max_length=255)
     session_creation_date = fields.DatetimeField(auto_now_add=True)
 
     class Meta:
@@ -13,5 +14,8 @@ class UserSession(Model):
         table_description = "Information regarding a user's session"
 
     def __str__(self) -> str:
-        message = f"Session {self.session_id} for user {self.user_id} created: {self.session_creation_date}"
+        message = f"Session {self.id} for user {self.user} created: {self.session_creation_date}"
         return message
+
+
+UserSession_Pydantic = pydantic_model_creator(UserSession, name="User Session")
