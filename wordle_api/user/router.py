5c43ba622d8e9ec02f35b178cbec1e5d57392c23
lpardey <lpardey@outlook.com>
2023-10-07 00:39:31 +0000

    update get user method update create user method update login method

--- wordle_api/user/router.py
+++ wordle_api/user/router.py
@@ -1,19 +1,20 @@
-from fastapi import APIRouter, Request, status
+from fastapi import APIRouter, status
 from fastapi.exceptions import HTTPException
 
 # from wordle_api.user.auth import authorized_endpoint
-from wordle_api.user.models import User
-from .store import (
-    SessionStore,
-    SessionStoreDict,
-    StoreExceptionAlreadyInUse,
-    StoreExceptionNotFound,
-    StoreExceptionUnexpected,
-    UserStoreDict,
-    UserStore,
-)
-from .schemas import SignUpRequest, LoginRequest, LoginResponse, SignUpResponse, UpdateRequest
-from wordle_api.auth.utils import authenticate_user, get_password_hash
+# from .store import (
+#     SessionStore,
+#     SessionStoreDict,
+#     StoreExceptionAlreadyInUse,
+#     StoreExceptionNotFound,
+#     StoreExceptionUnexpected,
+#     UserStoreDict,
+#     UserStore,
+# )
+from .schemas import SignUpRequest, LoginRequest, LoginResponse, SignUpResponse
+from wordle_api.auth.utils import AuthException, authenticate_user, get_password_hash
+from wordle_api.models.user import User, User_Pydantic
+from wordle_api.models.user_session import UserSession, UserSession_Pydantic
 
 router = APIRouter(prefix="/account", tags=["Account"])
 
@@ -21,87 +22,70 @@ router = APIRouter(prefix="/account", tags=["Account"])
 LOGIN_FAILED = HTTPException(status.HTTP_404_NOT_FOUND, detail="Incorrect username or password")
 
 
-@router.get("/name_availability")
-def is_user_available(username: str) -> None:
-    user_store: UserStore = UserStoreDict.get_instance()
-    if username in user_store.username_index:
-        raise HTTPException(status.HTTP_409_CONFLICT, detail=f"Username '{username}' is unavailable")
+# @router.put("/update/{user_id}")
+# # @authorized_endpoint
+# def update_user(request: Request, user_id: int, update_request: UpdateRequest) -> None:
+#     try:
+#         user_store: UserStore = UserStoreDict.get_instance()
+#         user = User(id=user_id, username=update_request.username, password=update_request.password)
+#         user_store.update_user(user)
+
+#     except StoreExceptionAlreadyInUse as e:
+#         raise HTTPException(status.HTTP_409_CONFLICT, detail=str(e))
+
+#     except StoreExceptionUnexpected as e:
+#         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
-@router.put("/update/{user_id}")
-# @authorized_endpoint
-def update_user(request: Request, user_id: int, update_request: UpdateRequest) -> None:
-    try:
-        user_store: UserStore = UserStoreDict.get_instance()
-        user = User(id=user_id, username=update_request.username, password=update_request.password)
-        user_store.update_user(user)
+# @router.delete("/delete/{user_id}")
+# # @authorized_endpoint
+# def delete_user(request: Request, user_id: int) -> None:
+#     try:
+#         user_store: UserStore = UserStoreDict.get_instance()
+#         user_store.delete_user(user_id)
 
-    except StoreExceptionAlreadyInUse as e:
-        raise HTTPException(status.HTTP_409_CONFLICT, detail=str(e))
+#     except StoreExceptionNotFound as e:
+#         raise HTTPException(status.HTTP_404_NOT_FOUND, detail=str(e))
 
-    except StoreExceptionUnexpected as e:
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-
-
-@router.delete("/delete/{user_id}")
-# @authorized_endpoint
-def delete_user(request: Request, user_id: int) -> None:
-    try:
-        user_store: UserStore = UserStoreDict.get_instance()
-        user_store.delete_user(user_id)
-
-    except StoreExceptionNotFound as e:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail=str(e))
-
-    except StoreExceptionUnexpected as e:
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+#     except StoreExceptionUnexpected as e:
+#         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
 @router.get("/get")
-def get_user(username: str) -> User:
-    try:
-        user_store: UserStore = UserStoreDict.get_instance()
-        user = user_store.get_user(username)
-        return user
-
-    except StoreExceptionNotFound as e:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail=str(e))
-
-    except StoreExceptionUnexpected as e:
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+async def get_user(username: str) -> User_Pydantic:
+    user_query = await User.get_or_none(username=username)
+    if user_query is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"Username '{username}' not found")
+    user = await User_Pydantic.from_tortoise_orm(user_query)
+    return user
 
 
 @router.post("/create")
-def create_user(request: SignUpRequest) -> SignUpResponse:
+async def create_user(request: SignUpRequest) -> SignUpResponse:
+    if await User.exists(username=request.username):
+        raise HTTPException(status.HTTP_409_CONFLICT, detail=f"Username '{request.username}' is unavailable")
     try:
-        user_store: UserStore = UserStoreDict.get_instance()
         hashed_password = get_password_hash(request.password)
-        player_id = user_store.create_user(request.username, hashed_password)
-        return SignUpResponse(player_id=player_id)
-
-    except StoreExceptionAlreadyInUse as e:
-        raise HTTPException(status.HTTP_409_CONFLICT, detail=str(e))
-
-    except StoreExceptionUnexpected as e:
+        user = await User.create(username=request.username, password=hashed_password)
+        return SignUpResponse(user_id=user.id)
+    except Exception as e:
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
 @router.post("/login")
-def login(request: LoginRequest) -> LoginResponse:
+async def login(request: LoginRequest):
     try:
-        user = authenticate_user(request.username, request.password)
-        session_store: SessionStore = SessionStoreDict.get_instance()
-        session_id = session_store.create_session(user.id)
-        session = session_store.get_session_by_id(session_id)
+        user = await authenticate_user(request.username, request.password)
+        token = "auth token"
+        session = await UserSession.create(user_id=user.id, token=token)
         return LoginResponse(
-            player_id=user.id,
-            session_id=session.session_id,
-            token=session.token,
-            session_expiration_date=session.expiration_date,
+            session_id=session.id,
+            user_id=session.user_id,  # DB-backing field: for some reason tortoise appends '_id' to FK fields by default.
+            token=token,
+            session_creation_date=session.session_creation_date,
         )
-
-    except StoreExceptionNotFound:
+        # return await UserSession_Pydantic.from_tortoise_orm(session) # TODO: Relations & Early-init isn't happening
+    except AuthException:
         raise LOGIN_FAILED
-
-    except StoreExceptionUnexpected as e:
+    except Exception as e:
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
