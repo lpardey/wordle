809c768f1dbbda76393ce0e16745300003c4141f
lpardey <lpardey@outlook.com>
2023-10-07 00:37:15 +0000

    refactor

--- wordle_api/database/initialize_db.py
+++ wordle_api/database/initialize_db.py
@@ -1,7 +1,8 @@
 from tortoise import Tortoise
 
 
-async def connect_to_db() -> None:
+async def initialize_db() -> None:
+    Tortoise.init_models(["wordle_api.models"], "models")
     await Tortoise.init(
         db_url="sqlite://db.sqlite3",
         modules={"models": ["wordle_api.models"]},
--- wordle_api/models/__init__.py
+++ wordle_api/models/__init__.py
@@ -1,6 +1,15 @@
-from .game import Game
-from .guess import Guess
-from .user import User
-from .user_session import UserSession
+from .game import Game, Game_Pydantic
+from .guess import Guess, Guess_Pydantic
+from .user import User, User_Pydantic
+from .user_session import UserSession, UserSession_Pydantic
 
-__all__ = ["Game", "Guess", "User", "UserSession"]
+__all__ = [
+    "Game",
+    "Guess",
+    "User",
+    "UserSession",
+    "Game_Pydantic",
+    "Guess_Pydantic",
+    "User_Pydantic",
+    "UserSession_Pydantic",
+]
