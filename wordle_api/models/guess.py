2847a9c5b57a14fa2260d1964a1fd74a3b4bd85b
lpardey <lpardey@outlook.com>
2023-10-07 00:38:08 +0000

    refactor guess model

--- wordle_api/models/guess.py
+++ wordle_api/models/guess.py
@@ -1,10 +1,11 @@
 from tortoise.models import Model
 from tortoise import fields
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class Guess(Model):
     id = fields.IntField(pk=True)
-    game_id = fields.ForeignKeyField("models.Game", related_name="guesses")
+    game = fields.ForeignKeyField("models.Game", related_name="guesses")
     value = fields.CharField(max_length=5)
 
     class Meta:
@@ -13,3 +14,6 @@ class Guess(Model):
 
     def __str__(self) -> str:
         return f"Guess {self.id} for game {self.game_id}: '{self.value}'"
+
+
+Guess_Pydantic = pydantic_model_creator(Guess, name="Guess")
