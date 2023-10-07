c1fc77b218de39e675150733ddc52adee0c9fcb0
lpardey <lpardey@outlook.com>
2023-10-07 00:38:35 +0000

    refactor user model

--- wordle_api/models/user.py
+++ wordle_api/models/user.py
@@ -2,12 +2,13 @@ from tortoise import fields
 from tortoise.models import Model
 from .game import Game
 from .user_session import UserSession
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class User(Model):
     id = fields.IntField(pk=True)
-    username = fields.CharField(max_length=20)
-    password = fields.CharField(max_length=20)
+    username = fields.CharField(max_length=255)
+    password = fields.CharField(max_length=255)
     disabled = fields.BooleanField(default=False)
 
     games: fields.ReverseRelation["Game"]
@@ -19,3 +20,6 @@ class User(Model):
 
     def __str__(self) -> str:
         return f"User {self.id}: '{self.username}'"
+
+
+User_Pydantic = pydantic_model_creator(User, name="User")
