5ce6dd78718e160c34ab66a74a938c04f6a9d610
lpardey <lpardey@outlook.com>
2023-10-07 00:35:46 +0000

    add method to register tortoise db add method to initialize db

--- wordle_api/main.py
+++ wordle_api/main.py
@@ -1,23 +1,29 @@
+from wordle_api.database.initialize_db import initialize_db
+from tortoise.contrib.fastapi import register_tortoise
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from wordle_api.game import router
+from wordle_api.game.router import router as game_router
 from wordle_api.user.router import router as user_router
 
 # from wordle_api.auth.asdf import router as auth_router
-# from .game.connect_to_db import connect_to_db
 
-app = FastAPI()
-# await connect_to_db()
-
-origins = ["*"]
-
+app = FastAPI(title="Wordlematic")
+initialize_db()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 app.include_router(user_router)
-app.include_router(router.router)
+app.include_router(game_router)
 # app.include_router(auth_router)
+
+register_tortoise(
+    app,
+    db_url="sqlite://db.sqlite3",
+    modules={"models": ["wordle_api.models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
