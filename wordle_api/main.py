8ed5447793b0ac8ed81f57e24a44686ee4e0dba4
lpardey <lpardey@outlook.com>
2023-10-09 22:05:18 +0000

    solved asyncio warning add on event decorator add startup_db

--- wordle_api/main.py
+++ wordle_api/main.py
@@ -2,13 +2,20 @@ from wordle_api.database.initialize_db import initialize_db
 from tortoise.contrib.fastapi import register_tortoise
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from wordle_api.game.router import router as game_router
-from wordle_api.user.router import router as user_router
+from wordle_api.routers.game_router import router as game_router
+from wordle_api.routers.user_router import router as user_router
+
 
 # from wordle_api.auth.asdf import router as auth_router
 
 app = FastAPI(title="Wordlematic")
-initialize_db()
+
+
+@app.on_event("startup")
+async def startup_db():
+    await initialize_db()
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
