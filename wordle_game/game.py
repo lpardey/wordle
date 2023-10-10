ac549a48214d3003d94cce026b6355de7c35dd11
lpardey <lpardey@outlook.com>
2023-10-10 00:05:57 +0000

    refactor

--- wordle_game/game.py
+++ wordle_game/game.py
@@ -1,13 +1,5 @@
-import time
-from wordle_game.game_state import (
-    GameResult,
-    GameState,
-    GameStatus,
-    GuessResult,
-    LetterStatus,
-)
 from wordle_client.game_word import AllWords
-
+from wordle_game.game_state import GameState, GameResult, GameStatus, LetterStatus, GuessResult
 
 # FIVE_LETTER_WORDS: list[str] = get_words_list()
 
@@ -17,8 +9,8 @@ class WordleException(Exception):
 
 
 class WordleGame:
-    def __init__(self, game_state: GameState) -> None:
-        self.game_state = game_state
+    def __init__(self, game: GameState) -> None:
+        self.game = game
 
     def guess(self, guess: str) -> GuessResult:
         self.validate_game_status()
@@ -26,36 +18,6 @@ class WordleGame:
         self.add_guess(guess=guess)  # If we reach this point, both the guess and the status are valid
         guess_result = self.update_game_state()
         return guess_result
-
-    # TODO: finish and check if it's going to be used
-    def new_game_countdown(self, time_in_seconds: int) -> None:
-        while time_in_seconds:
-            # minutes, seconds = divmod(time_in_seconds, 60)
-
-            # if minutes > 59:
-            #     hours, minutes = divmod(minutes, 60)
-            # else:
-            #     hours = 0
-
-            # print(f"{hours:02d}:{minutes:02d}:{seconds:02d}", end="\r")
-            time.sleep(1)
-            time_in_seconds -= 1
-
-        self.reset_game()
-
-    # TODO: finish and check if it's going to be used
-    def reset_game(self) -> None:
-        # Generar una nueva partida
-        #   Generar una nueva palabra
-        #   Resetar los attempts a 6
-        # self.game_state.status = GameStatus.WAITING_FOR_GUESS
-        # self.game_state.result = GameResult.DEFEAT
-        pass
-
-    # Auxiliary functions for guess ----------------------------------------------------------------------------------
-
-    # def add_guess_and_letter_status_result(guess: str, letter_status: list[LetterStatus]) -> list[tuple[str, list[LetterStatus]]]
-    # return list(zip(guess, letter_status))
 
     @staticmethod
     def validate_guess(guess: str) -> None:
@@ -69,33 +31,33 @@ class WordleGame:
             raise WordleException(f"Invalid guess: '{guess}' is not a word.")
 
     def validate_game_status(self) -> None:
-        if self.game_state.status == GameStatus.FINISHED:
+        if self.game.status == GameStatus.FINISHED:
             raise WordleException("Game is over!")
 
     def add_guess(self, guess: str) -> None:
         guess = guess.upper()
-        self.game_state.guesses.append(guess)
+        self.game.guesses.append(guess)
 
     def update_game_state(self) -> GuessResult:
         if self.is_victory():
-            self.game_state.result = GameResult.VICTORY
+            self.game.result = GameResult.VICTORY
             return GuessResult.GUESSED
 
         if self.is_defeat():
-            self.game_state.result = GameResult.DEFEAT
+            self.game.result = GameResult.DEFEAT
 
         return GuessResult.NOT_GUESSED
 
     def is_victory(self) -> bool:
-        if self.game_state.guesses == []:
+        if self.game.guesses == []:
             return False
 
-        word = self.game_state.game_word
-        guess = self.game_state.guesses[-1]
+        word = self.game.game_word
+        guess = self.game.guesses[-1]
         return word == guess
 
     def is_defeat(self) -> bool:
-        return self.game_state.guesses_left == 0
+        return self.game.guesses_left == 0
 
     @staticmethod
     def compare(guess: str, word: str) -> list[LetterStatus]:
