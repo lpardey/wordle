bf83482491bb9bf008d325781450f9423d2ab108
lpardey <lpardey@outlook.com>
2023-10-10 00:05:14 +0000

    refactor game schemas

---
+++ wordle_api/schemas/game_schemas.py
@@ -0,0 +1,59 @@
+from enum import Enum
+from pydantic import BaseModel
+from wordle_game.game import GameStatus
+from wordle_game.game_state import GameDifficulty, GuessResult, LetterStatus
+
+# schema = transferir informacion --- esto va en la API
+# modelo = almacenar informacion --- esto va en la base de datos
+
+
+class BasicStatus(str, Enum):
+    OK = "OK"
+    ERROR = "ERROR"
+
+
+class BasicResponse(BaseModel):
+    status: BasicStatus = BasicStatus.OK
+    message: str | None = None
+
+
+class GameConfig(BaseModel):
+    number_of_attempts: int = 6
+    game_difficulty: GameDifficulty = GameDifficulty.NORMAL
+
+
+class CreateGameRequest(BaseModel):
+    user_id: int
+    game_config: GameConfig
+
+
+class CreateGameResponse(BaseModel):
+    game_id: int
+
+
+class Guess(BaseModel):
+    word: str
+    letters_status: list[LetterStatus]
+
+
+class GameStatusInfo(BaseModel):
+    game_word: str
+    guesses: list[Guess]
+    attempts_left: int
+    game_status: GameStatus
+    difficulty: GameDifficulty
+
+
+class GameStatusResponse(BasicResponse):
+    game_status_info: GameStatusInfo
+
+
+class TakeAGuessRequest(BaseModel):
+    guess: str
+
+
+class TakeAGuessResponse(BasicResponse):
+    status: BasicStatus
+    message: str | None
+    guess_result: GuessResult | None
+    guess_letters_status: list[LetterStatus] | None
