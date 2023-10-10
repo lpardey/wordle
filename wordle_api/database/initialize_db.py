1154719fd2c1f782205fb5e8a1142e7e9adcc938
lpardey <lpardey@outlook.com>
2023-10-10 23:16:53 +0000

    work in progress

--- wordle_api/database/initialize_db.py
+++ wordle_api/database/initialize_db.py
@@ -2,8 +2,5 @@ from tortoise import Tortoise
 
 
 async def initialize_db() -> None:
-    Tortoise.init_models(["wordle_api.models"], "models")
-    await Tortoise.init(
-        db_url="sqlite://db.sqlite3",
-        modules={"models": ["wordle_api.models"]},
-    )
+    # Tortoise.init_models(["wordle_api.models"], "models")
+    await Tortoise.init(db_url="sqlite://db.sqlite3", modules={"models": ["wordle_api.models"]})
