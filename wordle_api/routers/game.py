f784bbfb7de61cbab38c361d5013f4f005fba483
lpardey <lpardey@outlook.com>
2023-10-24 23:39:52 +0000

    add logger remove get game by id function update get game status return refactor

--- wordle_api/routers/game.py
+++ wordle_api/routers/game.py
@@ -1,7 +1,6 @@
 from typing import Annotated
-from fastapi import Depends, HTTPException, APIRouter, status, Query
+from fastapi import Depends, HTTPException, APIRouter, status
 from wordle_api.models import Game, User
-from wordle_api.pydantic_models import Game_Pydantic
 from wordle_api.schemas import (
     BasicStatus,
     CreateGameRequest,
@@ -9,10 +8,13 @@ from wordle_api.schemas import (
     TakeAGuessRequest,
     TakeAGuessResponse,
 )
+from wordle_api.schemas.game import GameStatusResponse
 from wordle_api.services.authentication import get_current_active_user
 from wordle_client.game_word import AllWords, get_game_word
 from wordle_api.services.game import WordleException, WordleGame
+import logging
 
+logger = logging.getLogger("Game")
 
 router = APIRouter(prefix="/game", tags=["Game"])
 
@@ -30,27 +32,36 @@ async def create_game(current_user: Annotated[User, Depends(get_current_active_u
         )
         return CreateGameResponse(game_id=game.id)
     except Exception as e:
+        logger.exception(e)
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
-@router.get("/status", response_model=Game_Pydantic)
+@router.get("/status/{game_id}")
 async def get_game_status(
-    current_user: Annotated[User, Depends(get_current_active_user)],
-    game_id: int | None = Query(default=None, title="Game ID", description="Optional game ID"),
-):
-    game = await get_game_by_id(game_id=game_id, user=current_user)
+    game_id: int, current_user: Annotated[User, Depends(get_current_active_user)]
+) -> GameStatusResponse:
+    game = await Game.get_or_none(id=game_id)
     if game is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Game not found")
-    return await Game_Pydantic.from_tortoise_orm(game)
+    return GameStatusResponse(
+        id=game.id,
+        game_word=game.game_word,
+        guesses_left=await game.guesses_left,
+        status=await game.status,
+        result=await game.result,
+        difficulty=game.difficulty,
+        creation_date=game.creation_date,
+        guesses=await game.guesses.all().values_list("value", flat=True),
+    )
 
 
-@router.post("/guess")
+@router.post("/guess/{game_id}")
 async def take_a_guess(
+    game_id: int,
     current_user: Annotated[User, Depends(get_current_active_user)],
     guess_request: TakeAGuessRequest,
-    game_id: int = Query(default=None, title="Game ID", description="Optional game ID"),
 ) -> TakeAGuessResponse:
-    game = await get_game_by_id(game_id=game_id, user=current_user)
+    game = await Game.get_or_none(id=game_id)
     if game is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Game not found")
     guess = guess_request.guess.upper()
@@ -71,11 +82,3 @@ async def take_a_guess(
         guess_result=guess_result,
         guess_letters_status=guess_letters_status,
     )
-
-
-async def get_game_by_id(game_id: int | None, user: User) -> Game | None:
-    if game_id:
-        game = await Game.get_or_none(id=game_id)
-    else:
-        game = await user.games.order_by("-id").first()
-    return game
