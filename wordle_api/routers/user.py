79302ca06f92b4f0360373ac3e38010823e980e3
lpardey <lpardey@outlook.com>
2023-10-25 20:56:49 +0000

    add create user response schema

--- wordle_api/routers/user.py
+++ wordle_api/routers/user.py
@@ -1,8 +1,9 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, Path, status
 from fastapi.exceptions import HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from wordle_api.schemas import LoginResponse, SignUpRequest
+from wordle_api.schemas.user import CreateUserResponse
 from wordle_api.services.authentication import create_access_token, get_current_active_user
 from wordle_api.services.resources.utils import AuthException, get_password_hash, authenticate_user
 from wordle_api.pydantic_models import User_Pydantic
@@ -21,23 +22,27 @@ async def get_user(current_user: Annotated[User, Depends(get_current_active_user
 
 # Admin
 @router.get("/{username}", response_model=User_Pydantic)
-async def get_any_user(username: str):
+async def get_any_user(username: Annotated[str, Path(title="Username")]):
     user = await User.get_or_none(username=username)
     if user is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"User with {username=} not found")
     return await User_Pydantic.from_tortoise_orm(user)
 
 
-# TODO: change return object
-@router.post("/create", response_model=User_Pydantic)
-async def create_user(request: SignUpRequest):
+@router.post("/create", status_code=status.HTTP_201_CREATED)
+async def create_user(request: SignUpRequest) -> CreateUserResponse:
     if await User.exists(username=request.username):
         raise HTTPException(status.HTTP_409_CONFLICT, detail="Username unavailable")
     try:
         password = request.password.get_secret_value()
         password_hash = get_password_hash(password)
         user = await User.create(username=request.username, password_hash=password_hash)
-        return await User_Pydantic.from_tortoise_orm(user)
+        return CreateUserResponse(
+            user_id=user.id,
+            username=user.username,
+            disabled=user.disabled,
+            creation_date=user.creation_date,
+        )
     except Exception as e:
         logger.exception(e)
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
--- wordle_api/schemas/user.py
+++ wordle_api/schemas/user.py
@@ -11,9 +11,16 @@ class SignUpResponse(BaseModel):
     user_id: int
 
 
+class CreateUserResponse(BaseModel):
+    user_id: int
+    username: str
+    disabled: bool
+    creation_date: datetime
+
+
 class LoginResponse(BaseModel):
     session_id: int
     user_id: int
     access_token: str
-    token_type:str
+    token_type: str
     session_creation_date: datetime
