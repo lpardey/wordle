973175768272278a29cc766da1fe6f821a0b7ab6
lpardey <lpardey@outlook.com>
2023-10-25 20:55:55 +0000

    refactor

--- wordle_api/routers/game.py
+++ wordle_api/routers/game.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import Depends, HTTPException, APIRouter, status
+from fastapi import Depends, HTTPException, APIRouter, Path, status
 from wordle_api.models import Game, User
 from wordle_api.schemas import (
     BasicStatus,
@@ -30,7 +30,7 @@ async def create_game(current_user: Annotated[User, Depends(get_current_active_u
             max_attempts=create_game_request.game_config.number_of_attempts,
             difficulty=create_game_request.game_config.game_difficulty,
         )
-        return CreateGameResponse(game_id=game.id)
+        return CreateGameResponse(game_id=game.id, creation_date=game.creation_date)
     except Exception as e:
         logger.exception(e)
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
@@ -38,7 +38,8 @@ async def create_game(current_user: Annotated[User, Depends(get_current_active_u
 
 @router.get("/status/{game_id}")
 async def get_game_status(
-    game_id: int, current_user: Annotated[User, Depends(get_current_active_user)]
+    game_id: Annotated[int, Path(title="Game ID")],
+    current_user: Annotated[User, Depends(get_current_active_user)],
 ) -> GameStatusResponse:
     game = await Game.get_or_none(id=game_id)
     if game is None:
@@ -48,16 +49,17 @@ async def get_game_status(
         game_word=game.game_word,
         guesses_left=await game.guesses_left,
         status=await game.status,
-        result=await game.result,
         difficulty=game.difficulty,
         creation_date=game.creation_date,
         guesses=await game.guesses.all().values_list("value", flat=True),
+        result=await game.result,
+        finished_date=await game.finished_date,
     )
 
 
 @router.post("/guess/{game_id}")
 async def take_a_guess(
-    game_id: int,
+    game_id: Annotated[int, Path(title="Game ID")],
     current_user: Annotated[User, Depends(get_current_active_user)],
     guess_request: TakeAGuessRequest,
 ) -> TakeAGuessResponse:
@@ -69,10 +71,10 @@ async def take_a_guess(
     game_status = BasicStatus.OK
     message = None
     guess_result = None
-    guess_letters_status = None
+    letters_status = None
     try:
         guess_result = await wordle_game.guess(guess)
-        guess_letters_status = wordle_game.compare(guess, wordle_game.game.game_word)
+        letters_status = wordle_game.compare(guess, wordle_game.game.game_word)
     except WordleException as e:
         game_status = BasicStatus.ERROR
         message = str(e)
@@ -80,5 +82,5 @@ async def take_a_guess(
         status=game_status,
         message=message,
         guess_result=guess_result,
-        guess_letters_status=guess_letters_status,
+        letters_status=letters_status,
     )
