c11d09db5c3720bf73485fd29c6097ebd0475947
lpardey <lpardey@outlook.com>
2023-11-16 18:11:31 +0000

    add creation date field for querying purposes

--- api/v1/game/models/guess.py
+++ api/v1/game/models/guess.py
@@ -14,6 +14,7 @@ class Guess(Model):
     id = fields.UUIDField(pk=True)
     value = fields.JSONField()
     game: fields.ForeignKeyRelation["Game"] = fields.ForeignKeyField("models.Game", related_name="guesses")
+    creation_date = fields.DatetimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return f"Guess {self.id}: {self.value}"
