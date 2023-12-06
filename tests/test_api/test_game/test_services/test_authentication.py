e042619adca3070c0e184e761b51be0ca9de071e
lpardey <lpardey@outlook.com>
2023-12-06 22:22:59 +0000

    refactor test in favor of readability

--- tests/test_api/test_game/test_services/test_authentication.py
+++ tests/test_api/test_game/test_services/test_authentication.py
@@ -2,6 +2,7 @@
 from datetime import datetime, timedelta
 from unittest.mock import AsyncMock, Mock, patch
 
+
 # Dependencies
 import pytest
 from fastapi import HTTPException, status
@@ -20,26 +21,29 @@ from api.v1.user.models.user import User
         pytest.param(timedelta(minutes=10), id="Custom expire delta"),
     ],
 )
-@patch("api.v1.game.services.authentication.datetime")
 @patch.object(jwt, "encode")
-def test_create_access_token(
-    mock_encode: Mock, mock_datetime: Mock, expires_delta: timedelta | None, basic_app_settings: Settings
-):
+def test_create_access_token(mock_encode: Mock, expires_delta: timedelta | None, basic_app_settings: Settings):
     mock_encode.return_value = "test_encoded_token"
     test_data = {"sub": "test_username"}
-    fixed_datetime_now_value = datetime(2023, 12, 1)
-    mock_datetime.now.return_value = fixed_datetime_now_value
+    fixed_date = datetime(2023, 12, 1)
     expire = expires_delta if expires_delta else timedelta(minutes=basic_app_settings.ACCESS_TOKEN_LIFETIME)
+    with patch("api.v1.game.services.authentication.datetime") as mock_datetime:
+        mock_datetime.now.return_value = fixed_date
 
-    encoded_token = create_access_token(test_data, expires_delta)
-    test_data.update({"exp": mock_datetime.now() + expire})
+        encoded_token = create_access_token(test_data, expires_delta)
+        test_data.update({"exp": mock_datetime.now() + expire})
 
-    assert encoded_token == mock_encode.return_value
-    mock_encode.assert_called_once_with(
-        test_data,
-        basic_app_settings.SECRET_KEY,
-        basic_app_settings.TOKEN_ALGORITHM,
-    )
+        assert encoded_token == mock_encode.return_value
+        mock_encode.assert_called_once_with(
+            test_data,
+            basic_app_settings.SECRET_KEY,
+            basic_app_settings.TOKEN_ALGORITHM,
+        )
+        mock_datetime.now.assert_called_with()
+
+
+async def test_get_current_user():
+    ...
 
 
 @patch("api.v1.game.services.authentication.get_current_user")
@@ -50,7 +54,7 @@ async def test_get_current_active_user(mock_get_current_user: AsyncMock):
     result = await get_current_active_user(test_user)
 
     assert result == test_user
-    # await mock_get_current_user.assert_called_once_with(test_user)
+    # mock_get_current_user.assert_called_once_with(test_user)
 
 
 @patch("api.v1.game.services.authentication.get_current_user")
@@ -63,4 +67,4 @@ async def test_get_current_active_user_inactive_user(mock_get_current_user: Asyn
 
     assert exc.value.detail == "Inactive user"
     assert exc.value.status_code == status.HTTP_400_BAD_REQUEST
-    # mock_get_current_user.assert_awaited_once_with(test_user)
+    # mock_get_current_user.assert_called_once_with(test_user)
