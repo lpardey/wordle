f2778c2cd098347116a058ab07784b04e96b0904
lpardey <lpardey@outlook.com>
2023-12-08 23:42:05 +0000

    refactor test validate token invalid credentials and internal server error into a single one with pytest parametrize fix test get current user user is not found general refactor

--- tests/test_api/test_game/test_services/test_authentication.py
+++ tests/test_api/test_game/test_services/test_authentication.py
@@ -25,17 +25,16 @@ from api.v1.user.models.user import User
         pytest.param(timedelta(minutes=10), id="Custom expire delta"),
     ],
 )
-@patch.object(jwt, "encode")
+@patch.object(jwt, "encode", return_value="test_encoded_token")
 def test_create_access_token(mock_encode: Mock, expires_delta: timedelta | None, basic_app_settings: Settings):
-    mock_encode.return_value = "test_encoded_token"
     test_data = {"sub": "test_username"}
     fixed_date = datetime(2023, 12, 1)
-    expire = expires_delta if expires_delta else timedelta(minutes=basic_app_settings.ACCESS_TOKEN_LIFETIME)
+    expires_delta = expires_delta if expires_delta else timedelta(minutes=basic_app_settings.ACCESS_TOKEN_LIFETIME)
     with patch("api.v1.game.services.authentication.datetime") as mock_datetime:
         mock_datetime.now.return_value = fixed_date
 
         encoded_token = create_access_token(test_data, expires_delta)
-        test_data.update({"exp": mock_datetime.now() + expire})
+        test_data.update({"exp": mock_datetime.now() + expires_delta})
 
         assert encoded_token == mock_encode.return_value
         mock_encode.assert_called_once_with(
@@ -44,13 +43,12 @@ def test_create_access_token(mock_encode: Mock, expires_delta: timedelta | None,
             basic_app_settings.TOKEN_ALGORITHM,
         )
         mock_datetime.now.assert_called_with()
+        mock_datetime.now.call_count == 2
 
 
-@patch.object(jwt, "decode")
-@patch("api.v1.game.services.authentication.oauth2_scheme")
+@patch.object(jwt, "decode", return_value={"sub": "test_username"})
+@patch("api.v1.game.services.authentication.oauth2_scheme", return_value="123")
 def test_validate_token(mock_oauth2_scheme: Mock, mock_decode: Mock, basic_app_settings: Settings):
-    mock_oauth2_scheme.return_value = "123"
-    mock_decode.return_value = {"sub": "test_username"}
     token = mock_oauth2_scheme.return_value
     expected_result = mock_decode.return_value["sub"]
     mock_decode_args = (token, basic_app_settings.SECRET_KEY, [basic_app_settings.TOKEN_ALGORITHM])
@@ -62,38 +60,50 @@ def test_validate_token(mock_oauth2_scheme: Mock, mock_decode: Mock, basic_app_s
     # mock_oauth2_scheme.assert_called_once_with()
 
 
+@pytest.mark.parametrize(
+    "payload, side_effect, expected_exc_status_code, expected_exc_detail, expected_exc_headers",
+    [
+        pytest.param(
+            {},
+            None,
+            status.HTTP_401_UNAUTHORIZED,
+            "Invalid credentials",
+            {"WWW-Authenticate": "Bearer"},
+            id="Invalid Credentials",
+        ),
+        pytest.param(
+            None,
+            JWTError("Simulated unexpected error during decoding"),
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
+            "Something unexpected happened",
+            None,
+            id="Internal Server Error",
+        ),
+    ],
+)
 @patch.object(jwt, "decode")
-@patch("api.v1.game.services.authentication.oauth2_scheme")
-def test_validate_token_invalid_credentials(mock_oauth2_scheme: Mock, mock_decode: Mock, basic_app_settings: Settings):
-    mock_oauth2_scheme.return_value = "123"
-    mock_decode.return_value = {}
-    token = mock_oauth2_scheme.return_value
-    mock_decode_args = (token, basic_app_settings.SECRET_KEY, [basic_app_settings.TOKEN_ALGORITHM])
-
-    with pytest.raises(HTTPException) as exc:
-        validate_token(token)
-
-    assert exc.value.status_code == status.HTTP_401_UNAUTHORIZED
-    assert exc.value.detail == "Invalid credentials"
-    assert exc.value.headers == {"WWW-Authenticate": "Bearer"}
-    mock_decode.assert_called_once_with(*mock_decode_args)
-
-
-@patch.object(jwt, "decode")
-@patch("api.v1.game.services.authentication.oauth2_scheme")
-def test_validate_token_internal_server_error(
-    mock_oauth2_scheme: Mock, mock_decode: Mock, basic_app_settings: Settings
+@patch("api.v1.game.services.authentication.oauth2_scheme", return_value="123")
+def test_validate_token_raise_exc(
+    mock_oauth2_scheme: Mock,
+    mock_decode: Mock,
+    payload: dict | None,
+    side_effect: JWTError | None,
+    expected_exc_status_code: status,
+    expected_exc_detail: str,
+    expected_exc_headers: dict[str, str] | None,
+    basic_app_settings: Settings,
 ):
-    mock_oauth2_scheme.return_value = "123"
-    mock_decode.side_effect = JWTError("Simulated unexpected error during decoding")
     token = mock_oauth2_scheme.return_value
+    mock_decode.return_value = payload
+    mock_decode.side_effect = side_effect
     mock_decode_args = (token, basic_app_settings.SECRET_KEY, [basic_app_settings.TOKEN_ALGORITHM])
 
     with pytest.raises(HTTPException) as exc:
         validate_token(token)
 
-    assert exc.value.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
-    assert exc.value.detail == "Something unexpected happened"
+    assert exc.value.status_code == expected_exc_status_code
+    assert exc.value.detail == expected_exc_detail
+    assert exc.value.headers == expected_exc_headers
     mock_decode.assert_called_once_with(*mock_decode_args)
 
 
@@ -102,50 +112,54 @@ def test_validate_token_internal_server_error(
 async def test_get_current_user(mock_validate_token: AsyncMock, mock_get_or_none: AsyncMock, basic_user: User):
     mock_validate_token.return_value = basic_user.username
     mock_get_or_none.return_value = basic_user
+    username = mock_validate_token.return_value
+    expected_result = mock_get_or_none.return_value
 
-    result = await get_current_user(mock_validate_token.return_value)
+    result = await get_current_user(username)
 
-    assert result == mock_get_or_none.return_value
+    assert result == expected_result
     # mock_validate_token.assert_called_once()
-    mock_get_or_none.assert_called_once_with(username=basic_user.username)
+    mock_get_or_none.assert_called_once_with(username=username)
 
 
-@pytest.mark.skip("Must fix!")
-@patch.object(User, "get_or_none")
+@patch.object(User, "get_or_none", new_callable=AsyncMock, return_value=None)
 @patch("api.v1.game.services.authentication.validate_token")
 async def test_get_current_user_user_is_not_found(
     mock_validate_token: AsyncMock, mock_get_or_none: AsyncMock, basic_user: User
 ):
     mock_validate_token.return_value = basic_user.username
-    mock_get_or_none.return_value = None
+    username = mock_validate_token.return_value
 
     with pytest.raises(HTTPException) as exc:
-        await get_current_user(mock_validate_token.return_value)
+        await get_current_user(username)
 
-    assert exc.value.detail == f"User with {basic_user.username} not found"
+    assert exc.value.detail == f"User with {username=} not found"
     assert exc.value.status_code == status.HTTP_404_NOT_FOUND
     # mock_validate_token.assert_called_once()
-    # mock_get_or_none.assert_called_once_with(username=basic_user.username)
+    mock_get_or_none.assert_called_once_with(username=username)
 
 
 @patch("api.v1.game.services.authentication.get_current_user")
 async def test_get_current_active_user(mock_get_current_user: AsyncMock, basic_user: User):
     mock_get_current_user.return_value = basic_user
+    current_user = mock_get_current_user.return_value
+    expected_result = current_user
 
-    result = await get_current_active_user(mock_get_current_user.return_value)
+    result = await get_current_active_user(current_user)
 
-    assert result == mock_get_current_user.return_value
-    # mock_get_current_user.assert_called_once_with(basic_user)
+    assert result == expected_result
+    # mock_get_current_user.assert_called_once()
 
 
 @patch("api.v1.game.services.authentication.get_current_user")
 async def test_get_current_active_user_inactive_user(mock_get_current_user: AsyncMock, basic_user: User):
     basic_user.disabled = True
     mock_get_current_user.return_value = basic_user
+    current_user = mock_get_current_user.return_value
 
     with pytest.raises(HTTPException) as exc:
-        await get_current_active_user(mock_get_current_user.return_value)
+        await get_current_active_user(current_user)
 
     assert exc.value.detail == "Inactive user"
     assert exc.value.status_code == status.HTTP_400_BAD_REQUEST
-    # mock_get_current_user.assert_called_once_with(test_user)
+    # mock_get_current_user.assert_called_once_with(username)
