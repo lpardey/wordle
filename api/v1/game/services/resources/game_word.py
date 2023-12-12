4a2a1ea1180563186c3f5f5a65ec4c57a49da608
lpardey <lpardey@outlook.com>
2023-12-12 23:27:48 +0000

    remove unnecessary methods

--- api/v1/game/services/resources/game_word.py
+++ api/v1/game/services/resources/game_word.py
@@ -1,7 +1,6 @@
 # Standard Library
 import logging
 import re
-from functools import partial
 from random import randint
 from typing import Callable
 
@@ -23,30 +22,12 @@ class AllWords:
     @property
     def words(cls) -> list[str]:
         if cls._words is None:
-            cls._words = get_words_list()
+            cls._words = get_words_list(ALL_WORD_SOURCES)
 
         return cls._words
 
 
-def get_words_list() -> list[str]:
-    return get_words_list_v2(ALL_WORD_SOURCES)
-
-
-def get_words_list_v1() -> list[str]:
-    internet_words: list[str] = []
-
-    try:
-        internet_words = get_words_list_from_meaningpedia()
-    except Exception as e:
-        logger.exception(e)
-
-    local_words = get_words_list_from_static_content()
-    local_extra_words = get_words_list_from_static_content(STATIC_EXTRA_WORDS_LIST_FILE)
-    all_words = list(set(internet_words) | set(local_words) | set(local_extra_words))
-    return all_words
-
-
-def get_words_list_v2(words_source_functions: list[WordSourceFunction]) -> list[str]:
+def get_words_list(words_source_functions: list[WordSourceFunction]) -> list[str]:
     words_lists: list[list[str]] = []
     for source_function in words_source_functions:
         try:
@@ -67,27 +48,16 @@ def get_words_list_from_static_content(filename: str = STATIC_WORDS_LIST_FILE) -
 def get_words_list_from_meaningpedia() -> list[str]:
     url = "https://meaningpedia.com/5-letter-words?show=all"
     response = requests.get(url=url)
-
-    # get list of words by grabbing regex captures of response
-    # there's probably a far better way to do this by actually parsing the HTML
-    # response, but I don't know how to do that, and this gets the job done
-
-    # compile regex
-    pattern = re.compile(r'<span itemprop="name">(\w+)</span>')
-    # find all matches
-    words_list: list[str] = pattern.findall(response.text)
+    pattern = re.compile(r'<span itemprop="name">(\w+)</span>')  # Compile regex
+    words_list: list[str] = pattern.findall(response.text)  # Find all matches
     parsed_words_list = [word.upper() for word in words_list]
     return parsed_words_list
+
+
+ALL_WORD_SOURCES = [get_words_list_from_meaningpedia, get_words_list_from_static_content]
 
 
 def get_game_word(words_list: list[str]) -> str:
     index = randint(a=0, b=len(words_list))
     game_word = words_list[index]
     return game_word
-
-
-ALL_WORD_SOURCES = [
-    get_words_list_from_meaningpedia,
-    get_words_list_from_static_content,
-    partial(get_words_list_from_static_content, STATIC_EXTRA_WORDS_LIST_FILE),
-]
